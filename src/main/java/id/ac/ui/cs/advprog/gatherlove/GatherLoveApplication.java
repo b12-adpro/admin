17c485ebea7bd9b2e8f12613aa651f732d00c2f2
FransiscaEllyaB <ellyafransiscab@gmail.com>
2025-04-07 05:59:16 +0700

    Initialized Sprng Boot Project

---
+++ src/main/java/id/ac/ui/cs/advprog/gatherlove/GatherLoveApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.gatherlove;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatherLoveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatherLoveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/ac/ui/cs/advprog/gatherlove/GatherLoveApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.gatherlove;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatherLoveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
