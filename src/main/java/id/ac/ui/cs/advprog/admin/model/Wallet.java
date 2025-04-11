6f594e0fa82f356374c790c68d2da43f45855e32
FransiscaEllyaB <ellyafransiscab@gmail.com>
2025-04-11 14:50:22 +0700

    [GREEN] add wallet model

---
+++ src/main/java/id/ac/ui/cs/advprog/admin/model/Wallet.java
@@ -0,0 +1,39 @@
+package id.ac.ui.cs.advprog.admin.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name = "wallets")
+@NoArgsConstructor
+@AllArgsConstructor
+public class Wallet {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    private Double balance = 0.0;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+        updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
+}
